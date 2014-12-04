aee92a5dfe90d0ec0474e1a1a30acbda50fef4b2
SagehariMaiko <sagehari_m@welmo.co.jp>
2014-12-04 19:13:45 +0900

    login2

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,8 +7,17 @@ import java.util.*;
 
 import models.*;
 
+@Check("admin")
 @With(Secure.class)
 public class Application extends Controller {
+	
+	@Before
+	static void setConnectedUser() {
+		if(Security.isConnected()) {
+			User user = User.find("byEmail", Security.connected()).first();
+			renderArgs.put("user", user.fullname);
+		}
+	}
 	
 	public static void index() {
 		List<ServiceName> servicenames = ServiceName.findAll();
